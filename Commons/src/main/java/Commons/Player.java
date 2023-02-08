6af520a9b37b1c4575e1e5fa7987cdb9c736089b
VIO47 <vmmv10@yahoo.com>
2023-02-08 12:19:28 +0100

    Build gradle

--- Commons/src/main/java/Commons/Player.java
+++ Commons/src/main/java/Commons/Player.java
@@ -1,6 +1,5 @@
 package Commons;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +7,6 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity(name = "Player")
-@Table(name = "Player")
 @NoArgsConstructor
 @Setter
 @Getter
--- Server/src/main/java/Server/database/PlayerRepository.java
+++ Server/src/main/java/Server/database/PlayerRepository.java
@@ -2,6 +2,8 @@ package Server.database;
 
 import Commons.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 }
