5a8ad6535bab84052254e31b69d2d02edadcef72
VIO47 <vmmv10@yahoo.com>
2023-02-09 00:44:17 +0100

    Prepare game scene

--- Client/src/main/java/Client/Scenes/GameCtrl.java
+++ Client/src/main/java/Client/Scenes/GameCtrl.java
@@ -3,14 +3,29 @@ package Client.Scenes;
 
 import Client.Utils.ServerUtils;
 import com.google.inject.Inject;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class GameCtrl {
 
     private final MainCtrl mainCtrl;
     private final ServerUtils utils;
     private String word;
+
+    @FXML
+    private Label fill1;
+    @FXML
+    private Label fill2;
+    @FXML
+    private Label fill3;
+    @FXML
+    private Label fill4;
+    @FXML
+    private Label fill5;
 
     @FXML
     private Label box11;
@@ -24,8 +39,7 @@ public class GameCtrl {
     private Label box15;
 
     @FXML
-    private Label test;
-
+    private AnchorPane scenePane;
 
     @Inject
     public GameCtrl(MainCtrl mainCtrl, ServerUtils utils) {
@@ -36,10 +50,10 @@ public class GameCtrl {
 
     public void createGame(){
         utils.createGame();
+        word = utils.getWord();
     }
 
     public void showWord(){
-        word = utils.getWord();
         box11.setText(String.valueOf(word.charAt(0)));
         box12.setText(String.valueOf(word.charAt(1)));
         box13.setText(String.valueOf(word.charAt(2)));
@@ -47,5 +61,38 @@ public class GameCtrl {
         box15.setText(String.valueOf(word.charAt(4)));
     }
 
+    public void fillBoxes(){
+        EventHandler<KeyEvent> eventHandler = event -> {
+            if(!fill1.getText().equals("")){
+                fill2.setText(event.getText());
+            }
+            else if(!fill2.getText().equals("")){
+                fill3.setText(event.getText());
+            }
+            else if(!fill3.getText().equals("")){
+                fill4.setText(event.getText());
+            }
+            else if(!fill4.getText().equals("")){
+                fill5.setText(event.getText());
+            }
+        };
+        fill1.addEventHandler(KeyEvent.KEY_PRESSED, eventHandler);
+        fill1.addEventHandler(KeyEvent.KEY_TYPED, eventHandler);
+    }
 
+    public void color(){
+        EventHandler<MouseEvent> eventHandler = event -> {
+            fill1.setText("BOX1");
+            fill2.setText("BOX2");
+        };
+        scenePane.addEventHandler(MouseEvent.MOUSE_CLICKED, eventHandler);
+    }
+
+    public void resetAnswer(){
+        fill1.setText("");
+        fill2.setText("");
+        fill3.setText("");
+        fill4.setText("");
+        fill5.setText("");
+    }
 }
