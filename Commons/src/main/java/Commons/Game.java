d12d9d7b436f50b75a79aea9b88a053536707bca
VIO47 <vmmv10@yahoo.com>
2023-02-08 12:36:49 +0100

    Build gradle

---
+++ Commons/src/main/java/Commons/Game.java
@@ -0,0 +1,31 @@
+package Commons;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "Game")
+@NoArgsConstructor
+@Setter
+@Getter
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long playerId;
+    private String word;
+
+    public Game(Long id, Long playerId, String word){
+        this.id = id;
+        this.playerId = playerId;
+        this.word = word;
+    }
+}
---
+++ Commons/src/main/java/Commons/Player.java
@@ -0,0 +1,31 @@
+package Commons;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "Player")
+@NoArgsConstructor
+@Setter
+@Getter
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Long gameId;
+
+    public Player(Long id, String name){
+        this.id = id;
+        this.name = name;
+        this.gameId = 0L;
+    }
+}
